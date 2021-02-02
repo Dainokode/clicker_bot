4a1591c915c4704a1b1762a333ef1f8265d1a0cc
Antonio <antoniodainok@gmail.com>
2021-02-02 12:37:56 +0100

    final commit

--- main.py
+++ main.py
@@ -11,15 +11,29 @@ driver = webdriver.Chrome(chrome_driver_path)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 
-cookie = driver.find_element_by_css_selector("#cookie")
+cookie = driver.find_element_by_id("cookie")
+money = driver.find_element_by_id("money")
 
 
-timeout = time.time() + 60 * 1
+timeout = time.time() + 60 * 5
+time_after_5_seconds = time.time() + 5
 while True:
     cookie.click()
 
+
+    if time.time() > time_after_5_seconds:
+        upgrade_items = driver.find_elements_by_css_selector("#store div")
+        upgrade_id = [item.get_attribute("id") for item in upgrade_items]
+        for item in upgrade_items[::-1]:
+            if item.get_attribute("class") != "grayed":
+                item.click()
+                break
+
+        time_after_5_seconds += 5
 
     if time.time() > timeout:
         cookie_seconds = driver.find_element_by_css_selector("#cps")
         print(cookie_seconds.text)
         break
+
+
