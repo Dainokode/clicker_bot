6679273a4d13c4d9ab66b6a27daa1154075e8fb6
Antonio Daino <64027381+Dainokode@users.noreply.github.com>
2021-02-03 09:10:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
 
 
 chrome_driver_path = "C:\\Users\\amton\\Documents\\chromedriver.exe"
@@ -12,7 +11,6 @@ driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 
 cookie = driver.find_element_by_id("cookie")
-money = driver.find_element_by_id("money")
 
 
 timeout = time.time() + 60 * 5
@@ -28,9 +26,9 @@ while True:
             if item.get_attribute("class") != "grayed":
                 item.click()
                 break
-
         time_after_5_seconds += 5
 
+        
     if time.time() > timeout:
         cookie_seconds = driver.find_element_by_css_selector("#cps")
         print(cookie_seconds.text)
